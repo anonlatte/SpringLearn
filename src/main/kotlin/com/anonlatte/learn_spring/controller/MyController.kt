bc1a9792bd201c7a549d30b14ef77ce8420f3f6d
Herman Proshunin <g.proshunin15@gmail.com>
2023-09-10 14:20:01 +0500

    task 5

--- src/main/kotlin/com/anonlatte/learn_spring/controller/MyController.kt
+++ src/main/kotlin/com/anonlatte/learn_spring/controller/MyController.kt
@@ -1,7 +1,20 @@
 package com.anonlatte.learn_spring.controller
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
-@RestController
+@Controller
 class MyController {
+
+    @GetMapping("/hello")
+    fun showMyFirstPage(
+        @RequestParam(name = "name", required = false, defaultValue = "World")
+        name: String,
+        model: Model
+    ): String {
+        model.addAttribute("name", name)
+        return "my-first-page"
+    }
 }
